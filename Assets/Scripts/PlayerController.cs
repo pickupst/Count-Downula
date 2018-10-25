54aa82d32dbace813160c80920fa8681f2577338
pickupst <gd.okanuslu@gmail.com>
2018-10-25 17:08:37 +0300

    Create designe and player move

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Transform myTransform;
+
+	// Use this for initialization
+	void Start () {
+        myTransform = gameObject.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Move();
+       
+	}
+
+    private void Move()
+    {
+        //Sağa Gidecek
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            myTransform.position = new Vector2(myTransform.position.x + 2, myTransform.position.y);
+        }//Sola Gidecek
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            myTransform.position = new Vector2(myTransform.position.x - 2, myTransform.position.y);
+        }//Yukarı Gidecek
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            myTransform.position = new Vector2(myTransform.position.x, myTransform.position.y + 2);
+        }//Aşağı gidecek
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            myTransform.position = new Vector2(myTransform.position.x, myTransform.position.y - 2);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("gşrme");
+    }
+}
