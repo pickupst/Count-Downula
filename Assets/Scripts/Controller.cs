7da73c8839f9c68a37e9e0bf41ab83736f8dc4e9
pickupst <gd.okanuslu@gmail.com>
2018-10-28 07:44:10 +0300

    Kumda hareket kontrolü tamam , suda kayma hareketi tamam

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	void Start()
+	{
+		GetComponentInParent<PlayerController>().ust = "kum";
+		GetComponentInParent<PlayerController>().alt = "kum";
+		GetComponentInParent<PlayerController>().sag = "kum";
+		GetComponentInParent<PlayerController>().sol = "kum";
+		GetComponentInParent<PlayerController>().me = "kum";
+	}
+
+	 public void OnTriggerEnter2D(Collider2D col)
+    {
+		onControler(col);
+    }
+
+	private void onControler(Collider2D col) {
+
+			if ( this.gameObject.tag == "playerUst") {
+				
+				if (col.gameObject.tag == "zemin")
+					GetComponentInParent<PlayerController>().ust = "zemin";
+				else if (col.gameObject.tag == "kum")
+					GetComponentInParent<PlayerController>().ust = "kum";
+				else if (col.gameObject.tag == "water")
+					GetComponentInParent<PlayerController>().ust = "water";
+
+			} else if (this.gameObject.tag == "playerAlt") {
+
+				if (col.gameObject.tag == "zemin")
+					GetComponentInParent<PlayerController>().alt = "zemin";
+				else if (col.gameObject.tag == "kum")
+					GetComponentInParent<PlayerController>().alt  = "kum";
+				else if (col.gameObject.tag == "water")
+					GetComponentInParent<PlayerController>().alt = "water";
+
+			} else if (this.gameObject.tag == "playerSag")
+			{
+				if (col.gameObject.tag == "zemin")
+					GetComponentInParent<PlayerController>().sag = "zemin";
+				else if (col.gameObject.tag == "kum")
+					GetComponentInParent<PlayerController>().sag = "kum";
+				else if (col.gameObject.tag == "water")
+					GetComponentInParent<PlayerController>().sag = "water";
+			} else if (this.gameObject.tag == "playerSol")
+			{
+				if (col.gameObject.tag == "zemin")
+					GetComponentInParent<PlayerController>().sol = "zemin";
+				else if (col.gameObject.tag == "kum")
+					GetComponentInParent<PlayerController>().sol = "kum";
+				else if (col.gameObject.tag == "water")
+					GetComponentInParent<PlayerController>().sol = "water";
+			}else if (this.gameObject.tag == "me")
+			{
+				if (col.gameObject.tag == "zemin")
+					GetComponentInParent<PlayerController>().me = "zemin";
+				else if (col.gameObject.tag == "kum")
+					GetComponentInParent<PlayerController>().me = "kum";
+				else if (col.gameObject.tag == "water")
+					GetComponentInParent<PlayerController>().me = "water";
+			}
+
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,43 +4,128 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    private string direction;
+
+    public string me {private get;  set;}  
+	public string ust {private get;  set;}
+	public string alt {private get;  set;}
+	public string sag {private get;  set;}
+	public string sol {private get;  set;}
+
     Transform myTransform;
 
 	// Use this for initialization
 	void Start () {
+        direction = "stay";
         myTransform = gameObject.GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+        Debug.Log("yon : " + direction);
+        Debug.Log("ust : " + ust + " alt " + alt + " sag " + sag + " sol " + sol + " me " + me);
+
         Move();
        
 	}
+
+    private void FixedUpdate() {
+          sudaKay();
+    }
 
     private void Move()
     {
         //Sağa Gidecek
         if (Input.GetKeyDown(KeyCode.D))
         {
-            myTransform.position = new Vector2(myTransform.position.x + 2, myTransform.position.y);
+          moveRight();
         }//Sola Gidecek
         else if (Input.GetKeyDown(KeyCode.A))
         {
-            myTransform.position = new Vector2(myTransform.position.x - 2, myTransform.position.y);
+             moveLeft();
         }//Yukarı Gidecek
         else if (Input.GetKeyDown(KeyCode.W))
         {
-            myTransform.position = new Vector2(myTransform.position.x, myTransform.position.y + 2);
+             moveTop();
         }//Aşağı gidecek
         else if (Input.GetKeyDown(KeyCode.S))
         {
-            myTransform.position = new Vector2(myTransform.position.x, myTransform.position.y - 2);
+             moveBottom();
         }
+
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log("gşrme");
+    private void moveRight() {
+
+        if (sag == "kum") {
+            myTransform.Translate(new Vector2( 2, 0),Space.World);
+                    
+        }else if (sag == "water") {
+            direction = "sag";
+        }
+
     }
-}
+
+    private void moveLeft() {
+
+        if (sol == "kum") {
+            myTransform.Translate(new Vector2( -2, 0),Space.World);
+                    
+        }else if (sol == "water") {
+            direction = "sol";
+        }
+
+    }
+
+    private void moveTop() {
+
+        if (ust == "kum") {
+            myTransform.Translate(new Vector2( 0, 2),Space.World);
+                    
+        }else if (ust == "water") {
+            direction = "ust";
+        }
+
+    }
+
+    private void moveBottom() {
+
+        if (alt == "kum") {
+            myTransform.Translate(new Vector2( 0, -2),Space.World);
+                    
+        }else if (alt == "water") {
+            direction = "alt";
+        }
+
+    }
+    private void sudaKay() {
+        
+        if (direction == "sag")
+        {
+            if (sag == "water" || (sag == "kum" && me == "water"))
+                 myTransform.Translate(new Vector2(2 , 0), Space.World);
+            else
+                direction = "stay";
+        } else if (direction == "sol")
+        {
+            if (sol == "water" || (sol == "kum" && me == "water"))
+                 myTransform.Translate(new Vector2(-2 , 0), Space.World);
+            else
+                direction = "stay";
+        }else if (direction == "ust")
+        {
+            if (ust == "water" || (ust == "kum" && me == "water"))
+                 myTransform.Translate(new Vector2(0 , 2), Space.World);
+            else
+                direction = "stay";
+        }else if (direction == "alt")
+        {
+            if (alt == "water" || (alt == "kum" && me == "water"))
+                 myTransform.Translate(new Vector2(0 , -2), Space.World);
+            else
+                direction = "stay";
+        }
+       
+    }
+  }
